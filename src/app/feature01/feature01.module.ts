f30adba782a73b32fe221cbae58d402f0617c4eb
I354697 <miklos.karakas@sap.com>
2022-11-15 16:01:38 +0100

    add feature module

---
+++ src/app/feature01/feature01.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Feature01RoutingModule } from './feature01-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Feature01RoutingModule
+  ]
+})
+export class Feature01Module { }
